770f64c17d99e2a5f77db36224228a9ba4bfcad1
ArturSousa-Hub <artsousa09@gmail.com>
2026-03-05 16:05:43 -0300

    Update ContaBancariaTest.java

--- src/test/java/org/example/ContaBancariaTest.java
+++ src/test/java/org/example/ContaBancariaTest.java
@@ -8,24 +8,21 @@ class ContaBancariaTest {
 
     @Test
     void deveDepositarValorPositivoComSucesso() {
-        // Arrange
+    
         ContaBancaria conta = new ContaBancaria();
         double valorDeposito = 100.0;
 
-        // Act
         conta.depositar(valorDeposito);
-
-        // Assert
+        
         assertEquals(100.0, conta.getSaldo());
     }
 
     @Test
     void naoDevePermitirDepositoNegativoOuZero() {
-        // Arrange
+
         ContaBancaria conta = new ContaBancaria();
         double valorInvalido = 0.0;
 
-        // Act & Assert
         Exception exception = assertThrows(IllegalArgumentException.class, () -> {
             conta.depositar(valorInvalido);
         });
@@ -34,26 +31,24 @@ class ContaBancariaTest {
 
     @Test
     void deveSacarValorDisponivelComSucesso() {
-        // Arrange
+
         ContaBancaria conta = new ContaBancaria();
         conta.depositar(200.0);
         double valorSaque = 50.0;
 
-        // Act
         conta.sacar(valorSaque);
-
-        // Assert
+        
         assertEquals(150.0, conta.getSaldo());
     }
 
     @Test
     void naoDevePermitirSaqueMaiorQueSaldo() {
-        // Arrange
+
         ContaBancaria conta = new ContaBancaria();
         conta.depositar(100.0);
         double valorSaque = 150.0;
 
-        // Act & Assert
+
         Exception exception = assertThrows(IllegalArgumentException.class, () -> {
             conta.sacar(valorSaque);
         });
@@ -62,16 +57,16 @@ class ContaBancariaTest {
 
     @Test
     void deveRealizarMultiplasOperacoesCorretamente() {
-        // Arrange
+
         ContaBancaria conta = new ContaBancaria();
 
-        // Act
+
         conta.depositar(500.0);
         conta.sacar(100.0);
         conta.depositar(50.0);
         conta.sacar(450.0);
 
-        // Assert
+
         assertEquals(0.0, conta.getSaldo());
     }
 }
